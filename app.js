b723926f67cf41bd7a113237446f9c92e4163c6f
lrakarl <stephene.bernardo@gmail.com>
2019-01-23 22:42:16 +0800

    refactor app.js

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
+
 const http = require('http');
 const WebSocket = require('ws');
 var crypto = require("crypto");
+let WebsocketConnection = require('./WebsocketConnection')
 let registeredPathnames = new Map();
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  
-  if(req.url === '/createroom'){
-    res.writeHead(200, {'Content-Type': 'application/json'});
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+  if (req.url === '/createroom') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     var id = `${crypto.randomBytes(20).toString('hex')}`;
     const wss = new WebSocket.Server({ noServer: true });
-    setupWssConnection(wss);
-    registeredPathnames.set(id, wss);
+    let websocketConnection = new WebsocketConnection(wss,id)
+    registeredPathnames.set(id, websocketConnection);
     res.write(`{"roomid":"${id}"}`);
-  } else if(RegExp('^\/sendquestions\/\w*').test(req.url) && req.method == 'POST'){
-    var id = /(\/sendquestions)(\/\w*)/.exec(req.url)[2]
-
+  } else if (RegExp('^\/sendquestions\/\w*').test(req.url) && req.method == 'POST') {
+    var id = /(\/sendquestions)(\/\w*)/.exec(req.url)[2].replace('/', '')
     var body = '';
     req.on('data', function (data) {
-        body += data;
-        console.log("Partial body: " + body);
+      body += data;
     });
     req.on('end', function () {
-        console.log( registeredPathnames.get(id));
-        registeredPathnames.get(id).clients.forEach((function each(client) {
-          if ( client.readyState === WebSocket.OPEN) {
-            client.send(body);
-          }
-        }))
+      registeredPathnames.get(id).getConnection().clients.forEach((function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          registeredPathnames.get(id).addQuestions(body)
+          body.id = registeredPathnames.get(id).addQuestions(body)
+          console.log(body)
+          client.send(body);
+        }
+      }))
     });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-  }else{
+    res.write(`{}`);
+  } else if(RegExp('^\/sendanswer\/.+\/.+').test(req.url)){
+    console.log(/^\/sendanswer\/(.+)\/(.+)/.exec(req.url))
+    res.write('has url');
+  } else {
     res.write('no url');
-
   }
   res.end();
 });
 
 server.on('upgrade', function upgrade(request, socket, head) {
-  const pathname = (request.url).replace('/','');
+  const pathname = (request.url).replace('/', '');
   if (registeredPathnames.has(pathname)) {
     handleUpgrade(pathname, request, socket, head);
   } else {
@@ -50,20 +55,8 @@ server.on('upgrade', function upgrade(request, socket, head) {
 server.listen(8080);
 
 function handleUpgrade(pathname, request, socket, head) {
-  registeredPathnames.get(pathname).
+  registeredPathnames.get(pathname).getConnection().
     handleUpgrade(request, socket, head, function done(ws) {
-      registeredPathnames.get(pathname).emit('connection', ws, request);
+      registeredPathnames.get(pathname).getConnection().emit('connection', ws, request);
     });
-}
-
-function setupWssConnection(wss) {
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
-        }
-      });
-    });
-  });
 }
